03d858efc614ec600b7bbea499b738d41b8d00ca
myclamm <mikelam231@gmail.com>
2014-07-24 20:57:00 -0700

    Completed LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,47 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+     if (list.head === null && list.tail === null){
+        var headAndTail = makeNode(value);
+        this.head = headAndTail;
+        this.tail = headAndTail;
+      } else{
+        var temp = makeNode(value);
+       if (list.head.next === null){
+          this.head.next = temp;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+
+     }
+
+       //this.tail = makeNode(value);
+
   };
+
+
 
   list.removeHead = function(){
-  };
+    var oldHead = this.head;
+    var newHead = this.head.next;
+    delete oldHead;
+    this.head = newHead;
+    return oldHead.value;
+    };
 
   list.contains = function(target){
+    var solve = function(node){
+      if(node.value===target){
+        return true;
+      }else {
+         if(node.next != null){
+           return solve(node.next)
+         }
+      }
+      return false;
+    };s
+    return solve(this.head);
+
   };
 
   return list;
@@ -24,6 +59,8 @@ var makeNode = function(value){
   return node;
 };
 
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
