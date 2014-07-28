8b8595ca5000db2bdd427483b4c0351f8f0cc148
myclamm <mikelam231@gmail.com>
2014-07-28 11:46:20 -0700

    added bin tree and graph

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,79 @@
 var makeBinarySearchTree = function(value){
+  var tree = {
+    value : value,
+    left : {},
+    right: {},
+    insert:function(val){
+      //if value > tree.val, if left/right are empty.. then right = makeBinarySearchTree(value), else left = makeTree(value)
+      // else this.left.insert(value)
+      //
+      if(val < this.value){
+        if(this.left.value === undefined){ // if left is undefined
+          return this.left = makeBinarySearchTree(val);
+        } else {//if there's children
+          return this.left.insert(val);
+        }
+      }
+      if(val>this.value){
+
+        if(this.right.value === undefined){//if right is undefined
+          return this.right = makeBinarySearchTree(val);
+        } else { //if there's children
+          return this.right.insert(val);
+        }
+      }
+
+
+    },
+    depthFirstLog:function(callBack){
+      callBack(this.value);
+      if(this.left.left === undefined && this.right.right === undefined){
+        return;
+      }
+      if(this.left.value !== undefined){
+        this.left.depthFirstLog(callBack);
+      }
+      if(this.right.value !== undefined){
+        this.right.depthFirstLog(callBack);
+      }
+      else{
+        return 'there are no values';
+      }
+
+
+    },
+    contains:function(target){
+      if (this.value === undefined){
+        return false;
+      }
+      if (this.value === target){ //base case
+        return true;
+      }
+      if (this.left === undefined && this.right===undefined){ //if both are undefined, return false
+        return false;
+      }
+      else if (this.value > target){
+        if(this.left.left !== undefined){
+          return this.left.contains(target)
+        } else{
+          return false;
+        }
+      } else {
+        if(this.right.right !== undefined){return this.right.contains(target)}else{
+          return false;
+        }
+
+      }
+    }
+  };
+  return tree;
 };
+
+
+
+
+var binaryTree = makeBinarySearchTree(5)
+
 
 
 /*
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,43 @@
 var Graph = function(){
+  var newGraph = {};
+  return newGraph;
 };
 
+//A .addNode() method that takes a new node and adds it to the graph
 Graph.prototype.addNode = function(newNode, toNode){
-};
+  if(toNode){
+    this.toNode[newNode] = {};
+  }else{
+    this[newNode] = {};
+  }
 
+};
+// A .contains() method that takes any node and returns a boolean reflecting whether it can be found in the graph
 Graph.prototype.contains = function(node){
 };
+// A .removeNode() method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
 
 Graph.prototype.removeNode = function(node){
 };
+// A .getEdge() method that returns a boolean reflecting whether or not two nodes are connected
 
 Graph.prototype.getEdge = function(fromNode, toNode){
 };
+// A .addEdge() method that creates a edge (connection) between two nodes if they both are present within the graph
 
 Graph.prototype.addEdge = function(fromNode, toNode){
 };
+// A .removeEdge() method that removes the connection between two nodes
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
 };
 
+Graph.prototype.forEachNode = function (callback){
+
+};
+// A .forEachNode() method that traverses through the graph, calling a passed in function once on each node
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
